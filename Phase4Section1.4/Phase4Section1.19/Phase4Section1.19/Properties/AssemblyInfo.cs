6a1937d192fa66ae1e87ac4e39ee00e9c844cf3b
suryavamsi53 <63606535+Suryavamsi53@users.noreply.github.com>
2023-12-10 19:47:37 +0530

    Work on Multiple Data Using Tables

---
+++ Phase4Section1.4/Phase4Section1.19/Phase4Section1.19/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Phase4Section1.19")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phase4Section1.19")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f244f6cc-3b27-4793-b328-da66cffade32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Phase4Section1.4/Phase4Section1.19/Phase4Section1.19/TestFeature.feature
@@ -0,0 +1,9 @@
+Feature: Datatable in SpecFlow
+
+@datatable
+Scenario: Datatable Scenario 1
+	Given I enter below information in the login form
+	| email | password|
+	| one@dummy.com| password|
+	| two@dummy.com| password|
+	| three@dummy.com| password|
