15ca3ceda6bb0d21c68917e64e47bd4c949c08b8
suryavamsi53 <63606535+Suryavamsi53@users.noreply.github.com>
2023-12-10 19:18:26 +0530

    SpecFlow Setup

---
+++ Phase4Section1.4/Phase4Section1.18/Phase4Section1.18/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Phase4Section1.18")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Phase4Section1.18")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5907dbe-f9d6-410e-8321-c2ba313239a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
